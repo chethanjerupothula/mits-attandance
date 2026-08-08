c9a476ec5edd786d11021e444968141e11263ace
chethanjerupothula <chethanjerupothula@gmail.com>
2026-08-08 20:21:19 +0530

    Delete mits directory

--- mits/test_endpoints.py
+++
@@ -1,8 +0,0 @@
-import requests
-for path in ['/', '/status', '/results']:
-    try:
-        r = requests.get('http://127.0.0.1:8000' + path, timeout=5)
-        print(path, r.status_code)
-        print(r.text[:300])
-    except Exception as e:
-        print(path, 'ERROR', e)
